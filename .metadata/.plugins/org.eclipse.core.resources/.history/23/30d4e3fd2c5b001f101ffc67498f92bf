0f3d6d38b81e8c5ec044d66345d681ed0f666840
Leonardo Laranjeira <leonardolaranjeiradev@gmail.com>
2024-08-15 14:52:51 -0300

    Exercicio 2 finalizado

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/23/30d4e3fd2c5b001f101ffc67498f92bf
@@ -0,0 +1,11 @@
+package exercicio2;
+
+public class Soma_Vetor {
+
+	public static void main(String[] args) {
+		// Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida:
+		// Imprimir todos os elementos do vetor
+		// Mostrar na tela a soma e a média dos elementos do vetor
+	}
+
+}
--- Exercicios_Vetores/src/exercicio2/Soma_Vetor.java
+++ Exercicios_Vetores/src/exercicio2/Soma_Vetor.java
@@ -1,11 +1,48 @@
 package exercicio2;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Soma_Vetor {
 
 	public static void main(String[] args) {
 		// Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida:
 		// Imprimir todos os elementos do vetor
 		// Mostrar na tela a soma e a média dos elementos do vetor
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Quantos numeros voce vai digitar?");
+		int N = sc.nextInt();
+		double[] vet1 = new double[N];
+		double soma = 0;
+		double media = 0;
+		
+		for (int i = 0; i < N; i++) {
+			System.out.print("Digite um numero:");
+			vet1[i] = sc.nextDouble();
+		}
+		
+		System.out.println("VALORES = " + vet1[0] +"  "+ vet1[1] + "  " + vet1[2] + "  " + vet1[3]);
+		
+		for (int i = 0; i<N; i++) {
+			soma += vet1[i];
+		}
+		
+		media = soma / vet1.length;
+		System.out.printf("SOMA = %.2f%n", soma);
+		System.out.printf("MEDIA = %.2f%n", media);
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
 	}
 
 }
