fcfbcbe90ce7a52a202b8398a6610c6743b71809
Leonardo Laranjeira <leonardolaranjeiradev@gmail.com>
2024-08-16 22:04:18 -0300

    Criado exercicio 5

---
+++ Exercicios_Vetores/src/exercicio5/Maior_Posicao.java
@@ -0,0 +1,25 @@
+package exercicio5;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Maior_Posicao {
+
+	public static void main(String[] args) {
+		// Faça um programa que leia N números reais e armazene-os em um vetor. Em seguida, mostrar na tela
+		// o maior número do vetor (supor não haver empates). Mostrar também a posição do maior elemento,
+		// considerando a primeira posição como 0 (zero). 
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		sc.close();
+
+
+	}
+
+}
